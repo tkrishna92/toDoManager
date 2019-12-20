963ddba4aeb1df137633cfd4011947ce9fd56c95
tkrishna92 <tkrishnat92@gmail.com>
2019-12-20 18:30:12 +0530

    toDoManagerBE20

--- app/middleware/routeIpLogger.js
+++ app/middleware/routeIpLogger.js
@@ -13,24 +13,24 @@ let routeIpLogger = (req, res, next)=>{
         logger.info(`${req.method} method from ${remoteIP} for route ${req.originalUrl}`, "routeIpLogger", 5);
     }
 
-    // if(req.method === 'OPTIONS'){
+    if(req.method === 'OPTIONS'){
         
-    //     var headers = {};
-    //     // headers["Access-Control-Allow-Origin"]=req.headers.origin;
-    //     headers["Access-Control-Allow-Origin"]="*";
-    //     headers["Access-Control-Allow-Methods"]= "POST, GET, PUT, DELETE, OPTIONS";
-    //     headers["Access-Control-Allow-Credentials"] = false;
-    //     headers["Access-Control-Max-Age"]= '86400'
-    //     headers["Access-Control-Allow-Headers"] = "X-Requested-with, X-HTTP-Method-Override, Content-Type, Accept";
-    //     res.writeHead(200, headers);
-    //     res.end();
-    // }
-    // else {
-    //     res.header("Access-Control-Allow-Origin", "*");
-    //     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-    //     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    //     next();
-    // }
+        var headers = {};
+        // headers["Access-Control-Allow-Origin"]=req.headers.origin;
+        headers["Access-Control-Allow-Origin"]="*";
+        headers["Access-Control-Allow-Methods"]= "POST, GET, PUT, DELETE, OPTIONS";
+        headers["Access-Control-Allow-Credentials"] = false;
+        headers["Access-Control-Max-Age"]= '86400'
+        headers["Access-Control-Allow-Headers"] = "X-Requested-with, X-HTTP-Method-Override, Content-Type, Accept";
+        res.writeHead(200, headers);
+        res.end();
+    }
+    else {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        next();
+    }
 
 }
 
