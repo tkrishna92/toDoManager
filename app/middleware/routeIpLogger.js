2f05e4da7f7f0329b14ee62a8ca6974fa10d569d
tkrishna92 <tkrishnat92@gmail.com>
2019-12-20 17:51:27 +0530

    toDoManagerBE18

--- app/middleware/routeIpLogger.js
+++ app/middleware/routeIpLogger.js
@@ -17,7 +17,7 @@ let routeIpLogger = (req, res, next)=>{
         
         var headers = {};
         // headers["Access-Control-Allow-Origin"]=req.headers.origin;
-        // headers["Access-Control-Allow-Origin"]="*";
+        headers["Access-Control-Allow-Origin"]="*";
         headers["Access-Control-Allow-Methods"]= "POST, GET, PUT, DELETE, OPTIONS";
         headers["Access-Control-Allow-Credentials"] = false;
         headers["Access-Control-Max-Age"]= '86400'
@@ -26,7 +26,7 @@ let routeIpLogger = (req, res, next)=>{
         res.end();
     }
     else {
-        // res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Origin", "*");
         res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
         res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
         next();
